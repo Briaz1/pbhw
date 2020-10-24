0340edd9145d65d08e4674ceb941051caa957347
Briaz1 <ivakinda@list.ru>
2020-10-24 18:42:51 +0300

    Completed 6l 2t, upd 6l 1t

--- Lesson6/1TrafficLight.py
+++ Lesson6/1TrafficLight.py
@@ -1,16 +1,32 @@
-from time import sleep, time
+from time import sleep
 
 
 class TrafficLight:
-    color = {'Red': 7, 'Yellow': 2, 'Green': 12}
+    _color = {'Red': 7, 'Yellow': 2, 'Green': 12}
 
-    def running(self, work_time):
-        start = time()
-        while time() < start + work_time:
-            for colors in self.color:
+    def running(self):
+        while True:
+            for colors in self._color:
                 print(colors)
-                sleep(self.color.get(colors))
+                sleep(self._color.get(colors))
 
 
-t = TrafficLight()
-t.running(input("Enter (in sec) how long will traffic lights run: "))
+TrafficLight().running()
+
+
+# from time import sleep, time
+#
+#
+# class TrafficLight:
+#     _color = {'Red': 7, 'Yellow': 2, 'Green': 12}
+#
+#     def running(self, work_time):
+#         start = time()
+#         while time() < start + work_time:
+#             for colors in self._color:
+#                 print(colors)
+#                 sleep(self._color.get(colors))
+#
+#
+# t = TrafficLight()
+# t.running(float(input("Enter (in sec) how long will traffic lights run: ")))
---
+++ Lesson6/2road.py
@@ -0,0 +1,12 @@
+class Road:
+    _length = float(input('Enter road length (meters): '))   # meters
+    _width = float(input('Enter road width (meters):' ))  # meters
+
+    def asphalt_calc(self):
+        asp_mass = 0.025  # tons
+        asp_height = 5  # centimeters
+        return f'You need {self._length * self._width * asp_height * asp_mass} tons of asphalt'
+
+
+m = Road()
+print(m.asphalt_calc())
